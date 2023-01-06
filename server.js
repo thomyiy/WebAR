ecb7ab4b7c54f86ea8a973a162cc442fde48c51d
tho1294 <thom1205@gmail.com>
2023-01-06 17:38:18 +0100

    test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import https from 'https';
 import fs from 'fs';
 import bodyParser from 'body-parser';
 
-const options = {
+/*const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
-};
+};*/
 
 const PORT = 4200;
 
@@ -19,15 +19,15 @@ const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.json());
 
-/*app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Application started and Listening on port " + PORT);
-});*/
+});
 
 
 // serve your css as static
 app.use(express.static(__dirname));
 app.use(express.static('public'))
 
-https.createServer(options,app).listen(PORT, () => {
+/*https.createServer(options,app).listen(PORT, () => {
     console.log("Application started and Listening on port " + PORT);
-});
+});*/
